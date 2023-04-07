db14d821ab0270221a905b582c17a87329e518da
BrandonHVX <brandongines@gmail.com>
2023-04-07 07:13:37 -0400

    update

--- src/components/Slider/FullScreenVerticalSlider.jsx
+++ src/components/Slider/FullScreenVerticalSlider.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Mousewheel, Pagination, Navigation } from 'swiper';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+
 import { Link } from 'react-router-dom';
-import { Icon } from '@iconify/react';
 import parse from 'html-react-parser';
 import 'swiper/css';
 import 'swiper/css/pagination';
@@ -12,39 +10,12 @@ import Div from '../Div';
 
 
 
-function MyVerticallyCenteredModal(props) {
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-  );
-}
 
 
 
 
 export default function FullScreenVerticalSlider({ data }) {
-  const [modalShow, setModalShow] = React.useState(false);
+ 
   return (
     <>
       <Div className="cs-vertical_slider cs-swiper_arrow_style_1">
